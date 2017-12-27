73d6fafa8a006de6d6f3ab916cbcdc45b39f50ea
Asif Khan <asifkhan.myapps@gmail.com>
2017-12-27 21:07:06 +0600

    new activity created for completed todos

---
+++ app/src/main/java/com/example/asifkhan/sqlitesimpletodoapp/activities/CompletedTodos.java
@@ -0,0 +1,15 @@
+package com.example.asifkhan.sqlitesimpletodoapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.asifkhan.sqlitesimpletodoapp.R;
+
+public class CompletedTodos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_completed_todos);
+    }
+}

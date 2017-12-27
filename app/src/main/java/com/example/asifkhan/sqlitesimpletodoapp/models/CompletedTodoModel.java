e272af96f722adb9fe2448730f555c5c1416577e
Asif Khan <asifkhan.myapps@gmail.com>
2017-12-28 02:38:01 +0600

    new model created for completed todos

---
+++ app/src/main/java/com/example/asifkhan/sqlitesimpletodoapp/models/CompletedTodoModel.java
@@ -0,0 +1,97 @@
+package com.example.asifkhan.sqlitesimpletodoapp.models;
+
+/**
+ * Created by asifkhan on 12/27/17.
+ */
+
+public class CompletedTodoModel {
+    private int todoID;
+    private String todoTitle,todoContent,todoDate,
+            todoTimeFrom,toTimeTo,todoPriority,todoTag,todoColor;
+
+    public CompletedTodoModel(){}
+
+    public CompletedTodoModel(int todoID, String todoTitle, String todoContent, String todoDate, String todoTimeFrom, String toTimeTo, String todoPriority, String todoTag, String todoColor) {
+        this.todoID = todoID;
+        this.todoTitle = todoTitle;
+        this.todoContent = todoContent;
+        this.todoDate = todoDate;
+        this.todoTimeFrom = todoTimeFrom;
+        this.toTimeTo = toTimeTo;
+        this.todoPriority = todoPriority;
+        this.todoTag = todoTag;
+        this.todoColor = todoColor;
+    }
+
+    public int getTodoID() {
+        return todoID;
+    }
+
+    public void setTodoID(int todoID) {
+        this.todoID = todoID;
+    }
+
+    public String getTodoTitle() {
+        return todoTitle;
+    }
+
+    public void setTodoTitle(String todoTitle) {
+        this.todoTitle = todoTitle;
+    }
+
+    public String getTodoContent() {
+        return todoContent;
+    }
+
+    public void setTodoContent(String todoContent) {
+        this.todoContent = todoContent;
+    }
+
+    public String getTodoDate() {
+        return todoDate;
+    }
+
+    public void setTodoDate(String todoDate) {
+        this.todoDate = todoDate;
+    }
+
+    public String getTodoTimeFrom() {
+        return todoTimeFrom;
+    }
+
+    public void setTodoTimeFrom(String todoTimeFrom) {
+        this.todoTimeFrom = todoTimeFrom;
+    }
+
+    public String getToTimeTo() {
+        return toTimeTo;
+    }
+
+    public void setToTimeTo(String toTimeTo) {
+        this.toTimeTo = toTimeTo;
+    }
+
+    public String getTodoPriority() {
+        return todoPriority;
+    }
+
+    public void setTodoPriority(String todoPriority) {
+        this.todoPriority = todoPriority;
+    }
+
+    public String getTodoTag() {
+        return todoTag;
+    }
+
+    public void setTodoTag(String todoTag) {
+        this.todoTag = todoTag;
+    }
+
+    public String getTodoColor() {
+        return todoColor;
+    }
+
+    public void setTodoColor(String todoColor) {
+        this.todoColor = todoColor;
+    }
+}

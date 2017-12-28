25bb37f197c87f473ca00ca91be1a5fd9e058de5
Asif Khan <asifkhan.myapps@gmail.com>
2017-12-28 23:48:47 +0600

    option menu registered

--- app/src/main/java/com/example/asifkhan/sqlitesimpletodoapp/activities/AllTags.java
+++ app/src/main/java/com/example/asifkhan/sqlitesimpletodoapp/activities/AllTags.java
@@ -1,16 +1,21 @@
 package com.example.asifkhan.sqlitesimpletodoapp.activities;
 
 import android.os.Build;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.example.asifkhan.sqlitesimpletodoapp.R;
 
-public class AllTags extends AppCompatActivity {
+public class AllTags extends AppCompatActivity implements View.OnClickListener{
+    private FloatingActionButton fabAddTag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,34 @@ public class AllTags extends AppCompatActivity {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.setStatusBarColor(ContextCompat.getColor(this,R.color.statusBarColor));
+        }
+        fabAddTag=(FloatingActionButton)findViewById(R.id.fabAddTag);
+        fabAddTag.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.fabAddTag:
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_new_tag_option,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.search:
+                return true;
+            case R.id.setting:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }

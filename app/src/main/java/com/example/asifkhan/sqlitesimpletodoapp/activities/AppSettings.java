489f8ea17c24ecf8796b9d3b357725c4736654b6
Asif Khan <asifkhan.myapps@gmail.com>
2017-12-29 02:32:13 +0600

    new settings activity created

---
+++ app/src/main/java/com/example/asifkhan/sqlitesimpletodoapp/activities/AppSettings.java
@@ -0,0 +1,67 @@
+package com.example.asifkhan.sqlitesimpletodoapp.activities;
+
+import android.os.Build;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.asifkhan.sqlitesimpletodoapp.R;
+
+public class AppSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_settings);
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle(R.string.settings_title);
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+            Window window=this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(ContextCompat.getColor(this,R.color.statusBarColor));
+        }
+        getPrefFragment();
+        applySettings();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        applySettings();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        applySettings();
+    }
+
+    public static class AppPreference extends PreferenceFragment{
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.app_preferences);
+        }
+    }
+
+    //getting the setting fragment
+    private void getPrefFragment(){
+        FragmentManager fragmentManager=getFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.prefContainer,new AppPreference()).commit();
+    }
+
+    //apply app settings
+    private void applySettings(){
+
+    }
+}

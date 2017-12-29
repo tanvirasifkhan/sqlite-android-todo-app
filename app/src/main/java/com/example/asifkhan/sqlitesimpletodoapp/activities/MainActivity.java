fa81dc57d7f2fe5ee95bd90986a9fc06f48aa62a
Asif Khan <asifkhan.myapps@gmail.com>
2017-12-30 05:35:44 +0600

    new functionalities added for adding new todos

--- app/src/main/java/com/example/asifkhan/sqlitesimpletodoapp/activities/MainActivity.java
+++ app/src/main/java/com/example/asifkhan/sqlitesimpletodoapp/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.asifkhan.sqlitesimpletodoapp.activities;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -7,6 +9,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -22,12 +25,25 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.example.asifkhan.sqlitesimpletodoapp.R;
 import com.example.asifkhan.sqlitesimpletodoapp.adapters.PendingTodoAdapter;
+import com.example.asifkhan.sqlitesimpletodoapp.helpers.TagDBHelper;
+import com.example.asifkhan.sqlitesimpletodoapp.helpers.TodoDBHelper;
 import com.example.asifkhan.sqlitesimpletodoapp.models.PendingTodoModel;
+import com.example.asifkhan.sqlitesimpletodoapp.models.TagsModel;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
         View.OnClickListener{
@@ -36,6 +52,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private ArrayList<PendingTodoModel> pendingTodoModels;
     private PendingTodoAdapter pendingTodoAdapter;
     private FloatingActionButton addNewTodo;
+    private TagDBHelper tagDBHelper;
+    private String getTagTitleString;
+    private TodoDBHelper todoDBHelper;
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,20 +72,35 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     //loading all the pending todos
     private void loadPendingTodos(){
         pendingTodos=(RecyclerView)findViewById(R.id.pending_todos_view);
+        linearLayout=(LinearLayout)findViewById(R.id.no_pending_todo_section);
+        tagDBHelper=new TagDBHelper(this);
+        todoDBHelper=new TodoDBHelper(this);
+
+        if(todoDBHelper.countTodos()==0){
+            linearLayout.setVisibility(View.VISIBLE);
+            pendingTodos.setVisibility(View.GONE);
+        }else{
+            pendingTodoModels=new ArrayList<>();
+            pendingTodoModels=todoDBHelper.fetchAllTodos();
+            pendingTodoAdapter=new PendingTodoAdapter(pendingTodoModels,this);
+        }
         linearLayoutManager=new LinearLayoutManager(this);
-        pendingTodoModels=new ArrayList<>();
-        pendingTodoAdapter=new PendingTodoAdapter(pendingTodoModels,this);
         pendingTodos.setAdapter(pendingTodoAdapter);
         pendingTodos.setLayoutManager(linearLayoutManager);
         addNewTodo=(FloatingActionButton)findViewById(R.id.fabAddTodo);
         addNewTodo.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.fabAddTodo:
-                showNewTodoDialog();
+                if(tagDBHelper.countTags()==0){
+                    showDialog();
+                }else{
+                    showNewTodoDialog();
+                }
                 break;
         }
     }
@@ -158,22 +193,133 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
-    //show add new todo dialog
-    private void showNewTodoDialog(){
-        final AlertDialog.Builder builder=new AlertDialog.Builder(this);
-        LayoutInflater layoutInflater=(LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        final View view=layoutInflater.inflate(R.layout.add_new_todo_dialog,null);
-        builder.setView(view);
-        builder.setPositiveButton(R.string.add_todo_dialog_positive_text, new DialogInterface.OnClickListener() {
+    //show dialog if there is no tag in the database
+    private void showDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle(R.string.tag_create_dialog_title_text);
+        builder.setMessage(R.string.no_tag_in_the_db_text);
+        builder.setPositiveButton(R.string.create_new_tag, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-
+                startActivity(new Intent(MainActivity.this,AllTags.class));
             }
-        }).setNegativeButton(R.string.add_todo_dialog_cancel_text, new DialogInterface.OnClickListener() {
+        }).setNegativeButton(R.string.tag_edit_cancel, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
 
             }
         }).create().show();
+    }
+
+    //show add new todos dialog and adding the todos into the database
+    private void showNewTodoDialog(){
+        final AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        LayoutInflater layoutInflater=(LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View view=layoutInflater.inflate(R.layout.add_new_todo_dialog,null);
+        builder.setView(view);
+        final TextInputEditText todoTitle=(TextInputEditText)view.findViewById(R.id.todo_title);
+        final TextInputEditText todoContent=(TextInputEditText)view.findViewById(R.id.todo_content);
+        Spinner todoTags=(Spinner)view.findViewById(R.id.todo_tag);
+        //stores all the tags title in string format
+        ArrayAdapter<String> tagsModelArrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,tagDBHelper.fetchTagStrings());
+        //setting dropdown view resouce for spinner
+        tagsModelArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //setting the spinner adapter
+        todoTags.setAdapter(tagsModelArrayAdapter);
+        todoTags.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                getTagTitleString=adapterView.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        final TextInputEditText todoDate=(TextInputEditText)view.findViewById(R.id.todo_date);
+        final TextInputEditText todoTime=(TextInputEditText)view.findViewById(R.id.todo_time);
+
+        //getting current calendar credentials
+        final Calendar calendar=Calendar.getInstance();
+        final int year=calendar.get(Calendar.YEAR);
+        final int month=calendar.get(Calendar.MONTH);
+        final int day=calendar.get(Calendar.DAY_OF_MONTH);
+        final int hour=calendar.get(Calendar.HOUR);
+        final int minute=calendar.get(Calendar.MINUTE);
+
+        //getting the tododate
+        todoDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        todoDate.setText(i+"-"+i1+"-"+i2);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
+
+        //getting the todos time
+        todoTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog timePickerDialog=new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        Calendar newCalendar=Calendar.getInstance();
+                        newCalendar.set(Calendar.HOUR,i);
+                        newCalendar.set(Calendar.MINUTE,i1);
+                        String timeFormat=DateFormat.getTimeInstance(DateFormat.SHORT).format(newCalendar.getTime());
+                        todoTime.setText(timeFormat);
+                    }
+                },hour,minute,false);
+                timePickerDialog.show();
+            }
+        });
+        TextView cancel=(TextView)view.findViewById(R.id.cancel);
+        TextView addTodo=(TextView)view.findViewById(R.id.add_new_todo);
+        addTodo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //getting all the values from add new todos dialog
+                String getTodoTitle=todoTitle.getText().toString();
+                String getTodoContent=todoContent.getText().toString();
+                int todoTagID=tagDBHelper.fetchTagID(getTagTitleString);
+                String getTodoDate=todoDate.getText().toString();
+                String getTime=todoTime.getText().toString();
+
+                //checking the data fiels
+                boolean isTitleEmpty=todoTitle.getText().toString().isEmpty();
+                boolean isContentEmpty=todoContent.getText().toString().isEmpty();
+                boolean isDateEmpty=todoDate.getText().toString().isEmpty();
+                boolean isTimeEmpty=todoTime.getText().toString().isEmpty();
+
+                //adding the todos
+                if(isTitleEmpty){
+                    todoTitle.setError("Todo title required !");
+                }else if(isContentEmpty){
+                    todoContent.setError("Todo content required !");
+                }else if(isDateEmpty){
+                    todoDate.setError("Todo date required !");
+                }else if(isTimeEmpty){
+                    todoTime.setError("Todo time required !");
+                }else if(todoDBHelper.addNewTodo(
+                        new PendingTodoModel(getTodoTitle,getTodoContent,String.valueOf(todoTagID),getTodoDate,getTime)
+                )){
+                    Toast.makeText(MainActivity.this, R.string.todo_title_add_success_msg, Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this,MainActivity.class));
+                }
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,MainActivity.class));
+            }
+        });
+        builder.create().show();
     }
 }

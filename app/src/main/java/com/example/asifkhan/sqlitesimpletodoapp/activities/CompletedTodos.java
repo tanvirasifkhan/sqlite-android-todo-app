183ceffc790a0f27536833dffafce4ca599589de
Asif Khan <asifkhan.myapps@gmail.com>
2018-01-02 18:51:59 +0600

    theme functions bug fixed/modified

--- app/src/main/java/com/example/asifkhan/sqlitesimpletodoapp/activities/CompletedTodos.java
+++ app/src/main/java/com/example/asifkhan/sqlitesimpletodoapp/activities/CompletedTodos.java
@@ -159,16 +159,12 @@ public class CompletedTodos extends AppCompatActivity {
         SharedPreferences sharedPreferences=PreferenceManager.getDefaultSharedPreferences(this);
         String themeName=sharedPreferences.getString("app_theme","Default");
         if(themeName.equals("Default")){
-            setTheme(R.style.AppTheme);
             toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
         }else if(themeName.equals("Red")){
-            setTheme(R.style.RedTheme);
             toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimaryRed));
         }else if(themeName.equals("Green")){
-            setTheme(R.style.GreenTheme);
             toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimaryGreen));
         }else if(themeName.equals("Violete")){
-            setTheme(R.style.VioleteTheme);
             toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimaryViolete));
         }
     }
@@ -183,16 +179,12 @@ public class CompletedTodos extends AppCompatActivity {
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 
             if(themeName.equals("Default")){
-                setTheme(R.style.AppTheme);
                 window.setStatusBarColor(ContextCompat.getColor(this,R.color.colorPrimaryDark));
             }else if(themeName.equals("Red")){
-                setTheme(R.style.RedTheme);
                 window.setStatusBarColor(ContextCompat.getColor(this,R.color.colorPrimaryDarkRed));
             }else if(themeName.equals("Green")){
-                setTheme(R.style.GreenTheme);
                 window.setStatusBarColor(ContextCompat.getColor(this,R.color.colorPrimaryDarkGreen));
             }else if(themeName.equals("Violete")){
-                setTheme(R.style.VioleteTheme);
                 window.setStatusBarColor(ContextCompat.getColor(this,R.color.colorPrimaryDarkViolete));
             }
         }
